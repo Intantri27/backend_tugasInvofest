5b855b1ef282ab848081537e8d2d277cb16a732c
Intantri27 <trihartatiintan@gmail.com>
2026-05-23 13:48:05 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import Express from "express";
+import express from "express";
 import cors from "cors";
 import eventRoute from "./routes/eventRoute";
 import categoryRoute from "./routes/categoryRoute";
 import pembicaraRoute from "./routes/pembicaraRoutes";
 
-const app = Express();
+const app = express();
 const port = 3000;
-app.use(cors())
 
-
-app.use(Express.json());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Invofest");
+  res.send("Hello, World!");
 });
-app.use("/events", eventRoute);
-app.use("/categories", categoryRoute);
-app.use("/pembicara", pembicaraRoute);
+
+app.use('/events', eventRoute);
+app.use('/categories', categoryRoute);
+app.use('/pembicara', pembicaraRoute);
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
