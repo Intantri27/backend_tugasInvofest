e5a988f3e4807b264f6a74a4d1c06ad999e69a6d
Intantri27 <trihartatiintan@gmail.com>
2026-05-23 13:43:23 +0700

    benerin port

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import express from "express";
+import Express from "express";
 import cors from "cors";
 import eventRoute from "./routes/eventRoute";
 import categoryRoute from "./routes/categoryRoute";
 import pembicaraRoute from "./routes/pembicaraRoutes";
 
-const app = express();
-const port = process.env.PORT || 3000; // ← pakai env variable
+const app = Express();
+const port = 3000;
+app.use(cors())
 
-app.use(cors());
-app.use(express.json());
+
+app.use(Express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello Invofest");
 });
-
-app.use('/events', eventRoute);
-app.use('/categories', categoryRoute);
-app.use('/pembicara', pembicaraRoute);
+app.use("/events", eventRoute);
+app.use("/categories", categoryRoute);
+app.use("/pembicara", pembicaraRoute);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
